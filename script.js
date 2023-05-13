514a9c7538efcc2a08fc2681b8232d5b3cedbc68
SashaTrincher <oofman@ukr.net>
2023-05-13 17:26:19 +0200

    true button text update

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const trueButton = document.querySelector('.true-button');
 const falseButton = document.querySelector('.false-button');
 const mainContainer = document.querySelector('.global-container');
 const text = document.querySelector('.logo-inactive');
+const trueText = document.querySelector('.text-inactive');
 
 trueButton.addEventListener('click', () => {
     mainContainer.classList.replace('global-container', 'inactive');
+    trueText.classList.replace('text-inactive', 'text-active')
 });
 
 falseButton.addEventListener('click', () => {
